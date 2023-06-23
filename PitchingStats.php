1ae9bc2c05a41b351a5888540458aee38bf46097
CozmicTraveler <kazumarice@hotmail.co.jp>
2023-06-23 23:23:23 +0900

    Pitching stats's model

---
+++ PitchingStats.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PitchingStats extends Model
+{
+    use HasFactory;
+    public function getData(){
+        return $this->team . ',' .$this->name . ',' . $this->ip;
+    }
+}
