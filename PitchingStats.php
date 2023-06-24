38f8f595318ca10ee6dd194f14dffbf6ee3d7bc8
CozmicTraveler <kazumarice@hotmail.co.jp>
2023-06-24 14:31:39 +0900

    Using scope class

--- PitchingStats.php
+++ PitchingStats.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePitchingStats;
 
 class PitchingStats extends Model
 {
     use HasFactory;
+
+    protected static function boot(){
+        parent::boot();
+        static::addGlobalScope(new ScopePitchingStats);
+        // static::addGlobalScope('ip',function(Builder $builder){
+        //     $builder->where('ip','>=',30);
+        // });
+    }
+
     public function getData(){
         return $this->team . ',' .$this->name . ',' . $this->ip;
     }
