48028433ff5c19c90bf2f5f81dfae8164fece7dd
CozmicTraveler <kazumarice@hotmail.co.jp>
2023-06-29 23:21:02 +0900

    Modify apply method

--- ScopePitchingStats.php
+++ ScopePitchingStats.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
 class ScopePitchingStats implements Scope{
+  // public function apply(Builder $builder,Model $model){
+  //   $builder->where('ip','>=',30);
+  // }
   public function apply(Builder $builder,Model $model){
-    $builder->where('ip','>=',30);
+      return $builder->where('draft_year','2018');
   }
 }
